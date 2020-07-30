bd665762765765119957b6b3a7d1aaa8ecfa49b7
NathanOliveiraJF <nathanoliveira003jf@gmail.com>
2020-07-29 22:05:41 -0300

    Setup inicial do projeto

---
+++ src/components/Menu/components/ButtonLink/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function ButtonLink(props) {
+    return (
+        <a href={props.href} className={props.className}>
+            {props.children}
+        </a>
+    );
+}
+export default ButtonLink;
